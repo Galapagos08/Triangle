61bb7ff590511261b817420985891469b035b95a
Dan Esrey <danesrey@dans-air.lan>
2017-01-20 09:53:54 -0500

    Initial Commit

---
+++ Triangle/Triangle.h
@@ -0,0 +1,19 @@
+//
+//  Triangle.h
+//  Triangle
+//
+//  Created by Dan Esrey on 2017/20/01.
+//  Copyright © 2017 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Triangle.
+FOUNDATION_EXPORT double TriangleVersionNumber;
+
+//! Project version string for Triangle.
+FOUNDATION_EXPORT const unsigned char TriangleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Triangle/PublicHeader.h>
+
+
