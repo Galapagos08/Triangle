e941de9a3c7e5596ec776ccacaba7849bdf7d016
Dan Esrey <danesrey@dans-air.lan>
2017-01-20 09:55:55 -0500

    initial commit

---
+++ Triangle.swift
@@ -0,0 +1,13 @@
+//
+//  Triangle.swift
+//  Triangle
+//
+//  Created by Dan Esrey on 2017/20/01.
+//  Copyright © 2017 Dan Esrey. All rights reserved.
+//
+
+import Foundation
+
+class Triangle {
+    
+}
